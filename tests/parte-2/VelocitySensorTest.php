8a718fa8578907969a917966bc8b53be67225aeb
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-29 13:56:04 +0200

    add test for severe excess speed case

--- tests/parte-2/VelocitySensorTest.php
+++ tests/parte-2/VelocitySensorTest.php
@@ -4,25 +4,31 @@
 
   class VelocitySensorTest extends TestCase {
     
-    public function test_getSpeedLevel_lowSpeedCase(){
+    public function test_getSpeedLevel_lowSpeedCase() {
       $sensor =  new VelocitySensor(10);
       $this->assertEquals("Muy lento", $sensor->getSpeedLevel());
     }
 
-    public function test_getSpeedLevel_normalSpeedCase(){
+    public function test_getSpeedLevel_normalSpeedCase() {
       $sensor = new VelocitySensor(42);
       $this->assertEquals("Velocidad adecuada", $sensor->getSpeedLevel());
     }
 
-    public function test_getSpeedLevel_slightExcessSpeedCase(){
+    public function test_getSpeedLevel_slightExcessSpeedCase() {
       $sensor = new VelocitySensor(76);
       $this->assertEquals("Exceso leve", $sensor->getSpeedLevel());
     }
     
-    public function test_getSpeedLevel_moderateExcessSpeedCase(){
+    public function test_getSpeedLevel_moderateExcessSpeedCase() {
       $sensor = new VelocitySensor(91);
       $this->assertEquals("Exceso moderado", $sensor->getSpeedLevel());
+    } 
+
+    public function test_getSpeedLevel_severeExcessSpeedCase() {
+      $sensor = new VelocitySensor(200);
+      $this->assertEquals("Exceso moderado", $sensor->getSpeedLevel());
     }
+
   } 
      
 ?>
