c02b2fa1bb8788814f5c3ff178f36eec81518817
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-29 13:19:47 +0200

    create the VelocitySensor class and Make test pass for very slow speed

--- src/VelocitySensor.php
+++ src/VelocitySensor.php
@@ -1,3 +1,25 @@
 <?php
+namespace Src;
+
+class VelocitySensor  {
+    
+   private float $velocity;
+   
+   public function __construct(float $velocity) {
+      $this->velocity = $velocity;
+   }
+   
+   public function getSpeedLevel(){
+     if ($this->velocity < 30) {
+         return "Muy lento";
+     }
+   }
+
+
+}
+  
+
+
+
 
 ?>
