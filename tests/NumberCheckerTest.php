5c6fc4239c923e582004b76dd058b3b7119038a4
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-29 12:21:03 +0200

    Add unit tests for isEven method

---
+++ tests/NumberCheckerTest.php
@@ -0,0 +1,24 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use Src\NumberChecker;
+
+class NumberCheckerTest extends TestCase {
+     
+  public function test_isEven_par() {
+     $checker = new NumberChecker(10);
+     $this->assertTrue($checker->isEven());
+  }
+
+   public function test_isEven_impar() {
+    $checker = new NumberChecker(5);
+    $this->assertFalse($checker->isEven());
+}
+    
+
+
+
+
+
+
+
+?>
