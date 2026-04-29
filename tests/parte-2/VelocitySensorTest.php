88754b9f1bb8e8e5af750a2d17aa224472fe5a84
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-29 13:02:19 +0200

    Add test for very slow speed case

--- tests/parte-2/VelocitySensorTest.php
+++ tests/parte-2/VelocitySensorTest.php
@@ -1,3 +1,13 @@
 <?php
+  use PHPUnit\Framework\TestCase;
+  use Src\VelocitySensor;
 
+  class VelocitySensorTest extends TestCase {
+    
+    public function test_getSpeedLevel(){
+      $sensor =  new VelocitySensor(10);
+      $this->assertEquals("Muy lento", $sensor->getSpeedLevel());
+    }
+  }
+     
 ?>
