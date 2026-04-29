9aafe049b81aff1146e61b79366f3c17a382de31
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-29 14:01:18 +0200

    make test pass for severe excess speed case

--- src/VelocitySensor.php
+++ src/VelocitySensor.php
@@ -3,23 +3,25 @@ namespace Src;
 
 class VelocitySensor  {
     
-   private float $velocity;
+  private float $velocity;
    
-   public function __construct(float $velocity) {
-      $this->velocity = $velocity;
-   }
+  public function __construct(float $velocity) {
+     $this->velocity = $velocity;
+  }
    
-   public function getSpeedLevel(){
+  public function getSpeedLevel() {
      if ($this->velocity < 30) {
          return "Muy lento";
      } elseif ($this->velocity >= 30 && $this->velocity <= 60) {
                return "Velocidad adecuada";
      } elseif ($this->velocity > 60 && $this->velocity <= 80) {
                return "Exceso leve";
-     } elseif ($this->velocity > 80 && $this->velocity <= 100)
+     } elseif ($this->velocity > 80 && $this->velocity <= 100) {
                return "Exceso moderado";
-     
-}
+     } else {
+         return "Exceso grave";
+     }
+  }
   
 }
 
